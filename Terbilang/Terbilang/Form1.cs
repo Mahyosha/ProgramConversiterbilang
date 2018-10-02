b1fb01806611cc1ba66fed7a0e8be4072d5dd1c1
Mahyosha Muhammad <Mahyosha.muhammad@students.amikom.ac.id>
2018-10-02 17:36:07 +0700

    New post App
    Program conversi terbilang

---
+++ Terbilang/Terbilang/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary2;
+
+namespace Terbilang
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class2 conv = new Class2();
+            var hasil = conv.TerbilangIndonesia(int.Parse(textBox1.Text));
+            textBox2.Text = hasil.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
